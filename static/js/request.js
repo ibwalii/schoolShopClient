836a807a2827642a144b3b86bcec9817eaee13ef
ibwalii <ibrahimwali4u@gmail.com>
2020-10-10 13:26:59 +0100

    final design

---
+++ static/js/request.js
@@ -0,0 +1,25 @@
+$('#loginform').on('submit', function(){
+    event.preventDefault();
+    username = $('#username').val();
+    password = $('#password').val();
+    req = $.ajax({
+        url: 'http://localhost:5000/login',
+        type:'post',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify({username:username, password:password})
+    });
+    req.done(function(data){
+        
+        if (data.status == 'success'){
+            location.replace('dashboard.html');
+            localStorage.setItem('username', username);
+            localStorage.setItem('userID', data.user.id);
+            localStorage.setItem('salesToday', data.salesToday);
+            localStorage.setItem('allStudents', data.students);
+            localStorage.setItem('inactiveAccounts', data.inactiveAccounts);
+        }else{
+            alert('Invalid details');
+        }
+    });
+});
